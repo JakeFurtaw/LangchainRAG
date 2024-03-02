004cd9ccb07e6a247717d66f4eade1cc8d997488
JakeFurtaw <jfurtaw1@students.towson.edu>
2024-03-02 12:30:10 -0500

    added chroma db

--- text_chain.py
+++ text_chain.py
@@ -1,10 +1,13 @@
 from langchain_community.document_loaders import TextLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_chroma import Chroma
+from langchain_embeddings import OpenAIEmbeddings
 
 #path to the data
 DATA_PATH = 'data'
+CHROMA_PATH = 'chroma'
 
-#load the documents from .txt files
+#load the .txt files
 def load_docs():
     loader = TextLoader(DATA_PATH)
     documents = loader.load()
@@ -13,13 +16,13 @@ def load_docs():
 #split the documents into chunks of text
 def split_pages(documents):
     text_splitter = RecursiveCharacterTextSplitter(
-        separator='\n',
-        chunk_size=100,
-        chunk_overlap=25,
-        length_function=len,
-
+        chunk_size=100, chunk_overlap=25, length_function=len
     )
     chunks = text_splitter.split(documents)
     return chunks
+#save files to database
+def save_to_db(chunks):
 
-
+    db= Chroma.from_documents(
+        chunks, OpenAIEmbeddings(), persist_directory=CHROMA_PATH
+    )

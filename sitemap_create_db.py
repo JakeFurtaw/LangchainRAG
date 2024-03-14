40bb38dcd48a5337713c793be073416a0ade2952
JakeFurtaw <jfurtaw1@students.towson.edu>
2024-03-14 13:29:23 -0400

    created a sitemap version of the create_db file for testing purposes

--- sitemap_create_db.py
+++ sitemap_create_db.py
@@ -1,16 +1,16 @@
 """
-This script is used to create a database of text chunks from the .txt files in the data folder.
+This script is used to create a database of text chunks from a stiemap.
 It uses the langchain library to load douments, split text, embed the text, and save the text to a Chroma database.
 Then it creates a database of text chunks and saves them to a Chroma database.
 """
-from langchain_community.document_loaders import DirectoryLoader
+from langchain_community.document_loaders import SitemapLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.vectorstores.chroma import Chroma
 from langchain_community.embeddings import HuggingFaceEmbeddings
 import os
 import shutil
 
-DATA_PATH = 'data'
+SITEMAP_URL = 'https://www.towson.edu/sitemap.xml'
 CHROMA_PATH = 'chroma'
 EMBEDDING_MODEL = "intfloat/e5-large-v2"
 
@@ -19,14 +19,14 @@ def main():
     chunks = split_pages(documents)
     save_to_db(chunks)
 
-#load the .txt files
+# Load the documents from the sitemap.xml file
 def load_docs():
-    loader = DirectoryLoader(DATA_PATH)
+    loader = SitemapLoader(SITEMAP_URL)
     documents = loader.load()
     doc_text = [doc.page_content for doc in documents]
     return doc_text
 
-#split the documents into chunks of text
+# Split the documents into chunks of text
 def split_pages(doc_text):
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=250, 
@@ -36,13 +36,13 @@ def split_pages(doc_text):
     chunks = text_splitter.create_documents(doc_text)
     return chunks
 
-#save files to database
+# Save files to the database
 def save_to_db(chunks):
-    #clear db if it exists
+    # Clear the database if it exists
     if os.path.exists(CHROMA_PATH):
         shutil.rmtree(CHROMA_PATH)
-    #create new db from current docs
-    db= Chroma.from_documents(
+    # Create a new database from the current documents
+    db = Chroma.from_documents(
         chunks, HuggingFaceEmbeddings(), persist_directory=CHROMA_PATH
     )
     db.persist()

0aa99ad45ccca64322eb8083e06a3ef20c1290cc
JakeFurtaw <jfurtaw1@students.towson.edu>
2024-03-16 11:44:38 -0400

    trying to get sitemap loader to work

--- sitemap_create_db.py
+++ sitemap_create_db.py
@@ -9,19 +9,32 @@ from langchain.vectorstores.chroma import Chroma
 from langchain_community.embeddings import HuggingFaceEmbeddings
 import os
 import shutil
+import aiohttp
+import asyncio
+
 SITEMAP_URL = 'https://www.towson.edu/sitemap.xml'
 CHROMA_PATH = 'chroma'
 EMBEDDING_MODEL = "intfloat/e5-large-v2"
 def main():
-    documents = load_docs()
+    loop = asyncio.get_event_loop()
+    asyncio.set_event_loop(loop)
+    documents = loop.run_until_complete(load_docs())
     chunks = split_pages(documents)
     save_to_db(chunks)
 # Load the documents from the sitemap.xml file
-def load_docs():
+async def load_docs():
     loader = SitemapLoader(SITEMAP_URL)
-    documents = loader.load()
-    doc_text = [doc.page_content for doc in documents]
-    return doc_text
+    urls = [url for url in loader.sitemap_urls if not url.endswith(".pdf")]
+    doc_texts = []
+    for url in urls:
+        try:
+            doc = await loader._fetch_with_rate_limit(url)
+            doc_text = doc.page_content
+            doc_texts.append(doc_text)
+        except (UnicodeDecodeError, aiohttp.ContentTypeError):
+            # Skip non-text files
+            continue
+    return doc_texts
 # Split the documents into chunks of text
 def split_pages(doc_text):
     text_splitter = RecursiveCharacterTextSplitter(

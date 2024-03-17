0fea6958a30722f5064ecb9c81b26d370b7998d1
JakeFurtaw <jfurtaw1@students.towson.edu>
2024-03-17 12:14:33 -0400

    still fighting pdf error

--- sitemap_create_db.py
+++ sitemap_create_db.py
@@ -9,32 +9,20 @@ from langchain.vectorstores.chroma import Chroma
 from langchain_community.embeddings import HuggingFaceEmbeddings
 import os
 import shutil
-import aiohttp
-import asyncio
 
 SITEMAP_URL = 'https://www.towson.edu/sitemap.xml'
 CHROMA_PATH = 'chroma'
 EMBEDDING_MODEL = "intfloat/e5-large-v2"
 def main():
-    loop = asyncio.get_event_loop()
-    asyncio.set_event_loop(loop)
-    documents = loop.run_until_complete(load_docs())
+    documents = load_docs()
     chunks = split_pages(documents)
     save_to_db(chunks)
 # Load the documents from the sitemap.xml file
-async def load_docs():
+def load_docs():
     loader = SitemapLoader(SITEMAP_URL)
-    urls = [url for url in loader.sitemap_urls if not url.endswith(".pdf")]
-    doc_texts = []
-    for url in urls:
-        try:
-            doc = await loader._fetch_with_rate_limit(url)
-            doc_text = doc.page_content
-            doc_texts.append(doc_text)
-        except (UnicodeDecodeError, aiohttp.ContentTypeError):
-            # Skip non-text files
-            continue
-    return doc_texts
+    documents = loader.load()#Getting an error here, Cant decode PDF's. Need to figure out a work around.
+    doc_text = [doc.page_content for doc in documents]
+    return doc_text
 # Split the documents into chunks of text
 def split_pages(doc_text):
     text_splitter = RecursiveCharacterTextSplitter(

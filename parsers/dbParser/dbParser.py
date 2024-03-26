42f31b573bbb80d7c9e052117228a14269327f8f
JakeFurtaw <jfurtaw1@students.towson.edu>
2024-03-26 10:47:21 -0400

    parser is removing what i want it to keep. Needs some work...

--- parsers/dbParser/dbParser.py
+++ parsers/dbParser/dbParser.py
@@ -1,22 +1,27 @@
-from langchain.vectorstores import Chroma
+from langchain.vectorstores.chroma import Chroma
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain.docstore.document import Document
 import re
 
 CHROMA_PATH = 'chroma'
 CLEANED_DB_PATH = 'cleaned_chroma'
 
 # Load the Chroma database
-chroma_db = Chroma(persist_directory= CHROMA_PATH)
+chroma_db = Chroma(persist_directory=CHROMA_PATH)
 
-# Define the pattern to remove
-pattern = r'[\x80-\xff]|�|[� -�]'
+# Define the pattern
+pattern = r'[^a-zA-Z0-9\s\n]'
 
-# Iterate through the documents and remove the pattern
+# Iterate through the documents and remove everything expect the pattern
 updated_docs = []
-for doc in chroma_db.as_retriever():
-    cleaned_content = re.sub(pattern, '', doc.page_content, flags=re.UNICODE)
-    updated_doc = doc.copy(page_content=cleaned_content)
+for doc_tuple in chroma_db.as_retriever():
+    doc_content, doc_score = doc_tuple
+    cleaned_content = ''.join(re.findall(pattern, doc_content, flags=re.UNICODE))
+    metadata = {'score': str(doc_score)}  # Convert score to string
+    updated_doc = Document(page_content=cleaned_content, metadata=metadata)
     updated_docs.append(updated_doc)
 
 # Create a new Chroma database with the cleaned documents
-new_chroma_db = Chroma.from_documents(updated_docs, chroma_db.embedding_function, persist_directory='CLEANED_DB_PATH')
+new_chroma_db = Chroma.from_documents(
+    updated_docs, HuggingFaceEmbeddings(), persist_directory=CLEANED_DB_PATH)
 new_chroma_db.persist()

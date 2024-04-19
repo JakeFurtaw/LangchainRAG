455feb9b342ff16ce05c2f42769322782396b9f3
JakeFurtaw <jfurtaw97@gmail.com>
2024-04-19 16:48:16 -0400

    updated query file with better result printing and added chat template to tokenizer

--- oldFiles/query_db_with_top_K_print.py
+++ oldFiles/query_db_with_top_K_print.py
@@ -14,7 +14,7 @@ import sys
 from dotenv import load_dotenv
 from pathlib import Path
 
-# Path to the Chroma database and Embedding Model
+# Path to the Chroma database
 CHROMA_PATH = 'TowsonDB'
 EMBEDDING_MODEL = "BAAI/bge-large-en-v1.5"
 # Chat template to get better results from the model
@@ -50,9 +50,25 @@ def print_results(results):
         return
     print("\nResults:")
     print('-' * 80)
-    response_text = results[0]
-    print(response_text)
-    print('-' * 80)
+    for result in results:
+        if isinstance(result[0], str):
+            content = result[0].strip()
+        else:
+            content = result[0].page_content.strip()
+        score = result[1]
+        # Split the content into lines
+        lines = content.split('\n')
+        # Remove empty lines and leading/trailing whitespace
+        lines = [line.strip() for line in lines if line.strip()]
+        # Wrap long lines
+        wrapped_lines = []
+        for line in lines:
+            wrapped_lines.extend(wrap(line, width=80))
+        # Print the wrapped lines with a blank lines between each result
+        print('\n'.join(wrapped_lines))
+        print(f"Relevance Score: {score:.4f}")
+        print('-' * 80)
+
 # Main Function
 def main():
     # Load the database and the embedding function
@@ -64,9 +80,14 @@ def main():
         # Get the query
         query = ' '.join(sys.argv[1:])
         # Query the db for the most similar results
-        db.similarity_search_with_relevance_scores(query, k=5)
+        results = db.similarity_search_with_relevance_scores(query, k=5)
+        # Get the context for the chat prompt
+        docs = []
+        for result in results:
+            document, score = result
+            docs.append(document.page_content.strip())
         # Move the input tensors to the device
-        input_tensors = tokenizer(query, 
+        input_tensors = tokenizer(query,
                                   return_tensors="pt",
                                   padding=True).to(device)
         # Generate the response from the model
@@ -79,8 +100,9 @@ def main():
         # Print the results and query
         print('-' * 80)
         print(f"Query: {query}")
-        print_results([(f"\n"+response_text)])
+        print_results([(f"\n"+response_text, 1.0)])
     else:
         print("Please provide a query.")
+
 if __name__ == '__main__':
     main()

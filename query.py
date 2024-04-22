12ef7669f970e443c1dd31dab1bc2bc28d471f02
JakeFurtaw <jfurtaw97@gmail.com>
2024-04-22 17:42:18 -0400

    made some changes to the chat template, the way the reponse is printed out, and added conversation history to give the model memory

--- query.py
+++ query.py
@@ -9,6 +9,7 @@ from pathlib import Path
 CHROMA_PATH = 'TowsonDB'
 EMBEDDING_MODEL = "BAAI/bge-large-en-v1.5"
 MODEL_ID = "mistralai/Mixtral-8x22B-Instruct-v0.1"
+CONVERSATION_HISTORY = []
 CHAT_TEMPLATE = (
     "<s>[INST] <<SYS>>"
     "You are an AI Assistant that helps college students navigate Towson University campus. "
@@ -17,8 +18,6 @@ CHAT_TEMPLATE = (
     "Respond with clear, concise, and focused answers directly addressing the query. "
     "Use a positive and respectful tone suitable for college students. "
     "If you do not have enough information to answer a query, politely state that you are unable to provide a satisfactory answer."
-     "[/INST]{context_str} </s><s>[INST] {query}"
-    "<</SYS>>"
     "<<Example 1>>"
     "Query: What is the email address for Professor John Smith in the Computer Science department?"
     "Response: According to the information provided, the email address for Professor John Smith in the Computer Science department at Towson University is john.smith@towson.edu."
@@ -27,7 +26,9 @@ CHAT_TEMPLATE = (
     "Query: Where can I find information about on-campus housing?"
     "Response: For information about on-campus housing at Towson University, you can visit the Residence Life website at https://www.towson.edu/housing. This website provides details about the different residence halls, housing options, and the application process."
     "<<Example 2>>"
-    "[/INST]"
+    "<</SYS>>"
+    "<s>[INST] Context: {conversation_history}{context_str} Question: {query} Response: <[/INST]><RESPONSE>"
+
 )    
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -42,36 +43,34 @@ def print_results(query, response_text):
     if not response_text:
         print("Sorry, I couldn't find any relevant information for your query.")
         return
+    CONVERSATION_HISTORY.append(f"Query: {query}")
+    CONVERSATION_HISTORY.append(f"Response: {response_text}")
+    print(f"Query: {query}")
     print("\nResponse:")
     print('-' * 80)
-    print(f"Query: {query}")
     print(response_text)
     print('-' * 80)
 
 def get_relevant_documents(query, db):
-    search_results = db.similarity_search_with_relevance_scores(query, k=3)
+    search_results = db.similarity_search_with_relevance_scores(query, k=5)
     docs = []
     for result in search_results:
         document, score = result
         docs.append(document.page_content.strip())
-        print(f"Database Results:\n {document.page_content.strip()}")
-        print(f"Relevance score: {score}")
-        print("-" * 80)
+        # print(f"Database Results:\n {document.page_content.strip()}")
+        # print(f"Relevance score: {score}")
+        # print("-" * 80)
     return docs
 
 def generate_response(query, context_str):
-    input_text = CHAT_TEMPLATE.format(context_str=context_str, query=query)
-    
+    conversation_history = "\n".join(CONVERSATION_HISTORY)
+    input_text = CHAT_TEMPLATE.format(conversation_history = conversation_history, context_str=context_str, query=query)
     if tokenizer.pad_token is None:
         tokenizer.pad_token = tokenizer.eos_token
-    
     input_tensors = tokenizer(input_text, return_tensors="pt", padding=True).to(device)
     response = model.generate(**input_tensors, max_new_tokens=256, repetition_penalty=1.2, temperature=0.1, do_sample=True)
     response_text = tokenizer.decode(response[0], skip_special_tokens=True)
-
-    #Manually removing template from response
-    start_index = response_text.find("[INST]") + len("[/INST]")
-    response_text = response_text[start_index:].strip()
+    response_text = response_text.split('<RESPONSE>')[1]
     return response_text
 
 def main():

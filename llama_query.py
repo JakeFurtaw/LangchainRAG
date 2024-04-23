0a1e46f564bf7820f199e7ac835c35fb60a071dd
JakeFurtaw <jfurtaw97@gmail.com>
2024-04-23 17:42:34 -0400

    Had to roll back to older version one of the features i added made it stop working

--- llama_query.py
+++ llama_query.py
@@ -1,95 +1,92 @@
-"""
-This script is used to query the database of text chunks created in create_db.py.
-It uses the langchain library to load the model and tokenizer.
-Then the script queries the database and returns the result or results depending on the k #.
-MAKE SURE TO REIGNORE THE .env FILE AFTER USE
-"""
 from langchain.vectorstores.chroma import Chroma
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
-from textwrap import wrap
 import torch
 import os
 from dotenv import load_dotenv
 from pathlib import Path
 
-# Path to the Chroma database and Embedding Model
 CHROMA_PATH = 'TowsonDB'
 EMBEDDING_MODEL = "BAAI/bge-large-en-v1.5"
-# Chat template to get better results from the model
+MODEL_ID = "meta-llama/Meta-Llama-3-70B-Instruct"
+CONVERSATION_HISTORY = []
 CHAT_TEMPLATE = (
     "<s>[INST] <<SYS>>"
-    "You are an AI Assistant that helps college students navigate a college campus."
-    "Dont answer any questions about or give any information about any other school/university besides Towson University."
-    "You provide information like teacher and faculty contact information, teachers office room numbers, course information, enrollment information, campus resources," 
-    "and general campus information."
-    "Please ensure that your responses are clear, concise, and positive in nature."
-    "If you dont know the answer to a question, you can say that you are not sure."
+    "You are an AI Assistant that helps college students navigate Towson University campus. "
+    "Provide factual information based solely on the context given from the university's website. "
+    "Do not speculate or make up information if it is not covered in the context. "
+    "Respond with clear, concise, and focused answers directly addressing the query. "
+    "Use a positive and respectful tone suitable for college students. "
+    "If you do not have enough information to answer a query, politely state that you are unable to provide a satisfactory answer."
+    "<<Example 1>>"
+    "Query: What is the email address for Professor John Smith in the Computer Science department?"
+    "Response: According to the information provided, the email address for Professor John Smith in the Computer Science department at Towson University is john.smith@towson.edu."
+    "<<Example 1>>"
+    "<<Example 2>>"
+    "Query: Where can I find information about on-campus housing?"
+    "Response: For information about on-campus housing at Towson University, you can visit the Residence Life website at https://www.towson.edu/housing. This website provides details about the different residence halls, housing options, and the application process."
+    "<<Example 2>>"
     "<</SYS>>"
-    "[/INST] {context_str} </s><s>[INST] {query} [/INST]"
-)
-# Specify the GPU as device if available
-device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-# Load your Hugging Face API token
+    "<s>[INST] Context: {conversation_history}{context_str} Question: {query} Response: <[/INST]><RESPONSE>"
+)    
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 load_dotenv(Path(".env"))
 HUGGING_FACE_HUB_TOKEN = os.getenv("HUGGING_FACE_HUB_TOKEN")
-#Configure the quantization config
-quantization_config = BitsAndBytesConfig(load_in_8bit=True)
-# Load the Model and Tokenizer    
-tokenizer = AutoTokenizer.from_pretrained("meta-llama/Meta-Llama-3-70B", 
-    quantization_config=quantization_config,
-    chat_template=CHAT_TEMPLATE,
-    device_map="auto")
-model = AutoModelForCausalLM.from_pretrained("meta-llama/Meta-Llama-3-70B", 
-    quantization_config=quantization_config,
-    device_map="auto")
-# Printing Results to the CLI
-def print_results(results):
-    if not results:
+quantization_config = BitsAndBytesConfig(load_in_8bit=True, bnb_8bit_compute_dtype=torch.float16)
+tokenizer = AutoTokenizer.from_pretrained(MODEL_ID, quantization_config=quantization_config, device_map="auto")
+model = AutoModelForCausalLM.from_pretrained(MODEL_ID, quantization_config=quantization_config, device_map="auto")
+
+def print_results(query, response_text):
+    if not response_text:
         print("Sorry, I couldn't find any relevant information for your query.")
         return
-    print("\nResults:")
+    CONVERSATION_HISTORY.append(f"Query: {query}")
+    CONVERSATION_HISTORY.append(f"Response: {response_text}")
+    print(f"Query: {query}")
+    print("\nResponse:")
     print('-' * 80)
-    response_text = results[0]
-    print("\n"+response_text)
+    print(response_text)
     print('-' * 80)
-# Main Function
+
+def get_relevant_documents(query, db):
+    search_results = db.similarity_search_with_relevance_scores(query, k=5)
+    docs = []
+    for result in search_results:
+        document, score = result
+        docs.append(document.page_content.strip())
+        print(f"Database Results:\n {document.page_content.strip()}")
+        print(f"Relevance score: {score}")
+        print("-" * 80)
+    return docs
+
+def generate_response(query, context_str):
+    conversation_history = "\n".join(CONVERSATION_HISTORY)
+    input_text = CHAT_TEMPLATE.format(conversation_history = conversation_history, context_str=context_str, query=query)
+    if tokenizer.pad_token is None:
+        tokenizer.pad_token = tokenizer.eos_token
+    input_tensors = tokenizer(input_text, return_tensors="pt", padding=True).to(device)
+    response = model.generate(**input_tensors, max_new_tokens=256, repetition_penalty=1.2, top_p = .95, temperature=0.1, do_sample=True)
+    response_text = tokenizer.decode(response[0], skip_special_tokens=True)
+    response_text = response_text.split('<RESPONSE>')[1]
+    return response_text
+
 def main():
-    # Load the database and the embedding function
     embeddings = HuggingFaceEmbeddings(model_name=EMBEDDING_MODEL)
-    db = Chroma(persist_directory=CHROMA_PATH, 
-                embedding_function=embeddings)
+    db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embeddings)
+
     while True:
-        # Query the database
         query = input("Enter query: ")
         if len(query) == 0:
             print("Please enter a query.")
             continue
         elif query.lower() == "exit":
-                break
-        # Query the db for the most similar results
-        search_results=db.similarity_search_with_relevance_scores(query, k=5)
-        #combine search results from db search to form context string
-        docs = []
-        for result in search_results:
-            document, score = result
-            docs.append(document.page_content.strip())
-        context_str = "\n".join(docs)
-        print(context_str)
-        # Move the input tensors to the device
-        input_tensors = tokenizer(query,
-                                return_tensors="pt").to(device)
-        # Generate the response from the model
-        response = model.generate(**input_tensors, 
-                                    max_new_tokens=512,
-                                    temperature= .1,
-                                    do_sample=True)
-        response_text = tokenizer.decode(response[0], 
-                                        skip_special_tokens=True)
-        # Print the results and query
-        print('-' * 80)
-        print(f"Query: {query}")
-        print_results([(f"\n"+response_text)])
-        print(f"Relevance Score:{score:.4f}")
+            break
+
+        docs = get_relevant_documents(query, db)
+        context_str = "\n\n".join(docs)
+        response_text = generate_response(query, context_str)
+        print_results(query, response_text)
+
 if __name__ == '__main__':
     main()

b50aafd11581718205c35316c91cbd0b2c464aa8
JakeFurtaw <jfurtaw97@gmail.com>
2024-04-25 13:37:22 -0400

    still not working with sitemap loader. working locally with local parser

--- parsers/DivParser/sitemap_div_counter.py
+++ parsers/DivParser/sitemap_div_counter.py
@@ -6,25 +6,25 @@ SITEMAP = "https://www.towson.edu/sitemap.xml"
 loader = SitemapLoader(SITEMAP, continue_on_failure=True)
 documents = loader.load()
 
-div_info = []
+div_classes = []
+div_ids = []
 
 for document in documents:
-    soup = BeautifulSoup(document.page_content, 'html.parser')
+    content= document.page_content
+    print(content[:100])
+    soup = BeautifulSoup(content, 'html.parser')
     divs = soup.find_all('div')
     for div in divs:
-        div_class = div.get('class', [])
-        div_id = div.get('id', [])
-        div_info.append({
-            'class': div_class,
-            'id': div_id
-        })
+        if div.get('class'):
+            div_classes.extend(div.get('class'))
+        if div.get('id'):
+            div_ids.append(div.get('id'))
 
-# Save div_info to a text file
-with open('div_info.txt', 'w', encoding='utf-8') as file:
-    for div in div_info:
-        class_names = ', '.join(div['class'])
-        div_id = div['id'][0] if div['id'] else ''
-        file.write(f"Class: {class_names}, ID: {div_id}\n")
+print(f"Number of div classes: {len(div_classes)}")
+print(f"Number of div ids: {len(div_ids)}")
+print("Div classes:", div_classes)
+print("Div ids:", div_ids)
+
 
 
 

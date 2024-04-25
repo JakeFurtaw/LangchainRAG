8e29e91ab593568c7067e7e54b179c47b608e9b8
JakeFurtaw <jfurtaw97@gmail.com>
2024-04-25 10:23:04 -0400

    updated sitempa div counter to try and fix the issue of it only scanning one doc instead of all docs from sitemap

--- parsers/DivParser/sitemap_div_counter.py
+++ parsers/DivParser/sitemap_div_counter.py
@@ -6,30 +6,30 @@ def find_repeating_div_names(sitemap_url, output_file):
     loader = SitemapLoader(sitemap_url, continue_on_failure=True)
     docs = loader.load()
 
-    all_div_attributes = []
+    all_divs = []
 
     for doc in docs:
         html_content = doc.page_content
         soup = BeautifulSoup(html_content, "html.parser")
         divs = soup.find_all("div")
-        
-        div_attributes = []
-        for div in divs:
-            class_names = div.get('class', [])
-            id_name = div.get('id', '')
-            div_attributes.append({
-                'class': class_names,
-                'id': id_name
-            })
-        
-        all_div_attributes.extend(div_attributes)
+        all_divs.extend(divs)
 
-    print("Number of div elements found:", len(all_div_attributes))
-    print("Number of unique div classes found:", len(set(attr['class'] for attr in all_div_attributes)))
-    print("Number of unique div IDs found:", len(set(attr['id'] for attr in all_div_attributes if attr['id'])))
+    print("Number of div elements found:", len(all_divs))
 
-    class_counter = Counter(cls for attr in all_div_attributes for cls in attr['class'])
-    id_counter = Counter(attr['id'] for attr in all_div_attributes if attr['id'])
+    div_attributes = []
+    for div in all_divs:
+        class_names = div.get('class', [])
+        id_name = div.get('id', '')
+        div_attributes.append({
+            'class': class_names,
+            'id': id_name
+        })
+
+    print("Number of unique div classes found:", len(set(attr['class'] for attr in div_attributes)))
+    print("Number of unique div IDs found:", len(set(attr['id'] for attr in div_attributes if attr['id'])))
+
+    class_counter = Counter(cls for attr in div_attributes for cls in attr['class'])
+    id_counter = Counter(attr['id'] for attr in div_attributes if attr['id'])
 
     with open(output_file, "w") as file:
         file.write("Repeating class names:\n")
